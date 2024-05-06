dde8add451512ef58b0f5eedf1c9a0a3bd6bfd03
Atanu Mondal <mondalatanu1998@gmail.com>
2024-05-06 16:19:25 +0530

    status checker script added

---
+++ service_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "i am working!"
+
+status=`systemctl status jenkins.service | awk NR==3 | cut -d ":" -f 2 | cut -d "(" -f 1`
+
+#echo $status
+
+if [[ $status="active" ]]; then
+ echo "jenkins is running!"
+
+else
+    echo "jenkis is not running!"
+    echo "_______________________"
+    echo "starting jenkins service now!!"
+    systemctl start jenkins.service
+
+fi
